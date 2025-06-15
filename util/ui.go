e31f50a7b19039832ce9b652cb8e903aeae5480f
Nichtigott <38689145+Nichtigott@users.noreply.github.com>
2025-06-15 16:51:28 +0800

    Update ui.go
    Delet green

--- util/ui.go
+++ util/ui.go
@@ -53,10 +53,12 @@ func initPrimaryColor() {
 func GetRandomRgbColor(isRange bool) (string, string) {
 	rand.New(rand.NewSource(time.Now().UnixNano()))
 	r := 255 - rand.Intn(100)
+	
 	rand.New(rand.NewSource(time.Now().UnixNano() / 2))
-	g := 255 - rand.Intn(100)
-	rand.New(rand.NewSource(time.Now().UnixNano() / 3))
 	b := 255 - rand.Intn(100)
+
+	rand.New(rand.NewSource(time.Now().UnixNano() / 3))
+	g := min(r,b)
 
 	startColor := fmt.Sprintf("#%x%x%x", r, g, b)
 	if !isRange {
@@ -66,9 +68,9 @@ func GetRandomRgbColor(isRange bool) (string, string) {
 	rand.New(rand.NewSource(time.Now().UnixNano() / 5))
 	rEnd := 50 + rand.Intn(100)
 	rand.New(rand.NewSource(time.Now().UnixNano() / 7))
-	gEnd := 50 + rand.Intn(100)
-	rand.New(rand.NewSource(time.Now().UnixNano() / 11))
 	bEnd := 50 + rand.Intn(100)
+	rand.New(rand.NewSource(time.Now().UnixNano() / 11))
+	gEnd := min(rEnd, bEnd)
 	endColor := fmt.Sprintf("#%x%x%x", rEnd, gEnd, bEnd)
 
 	return startColor, endColor
